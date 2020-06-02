7ad7d761ed8b3f4862f3812e4c6877b9d145b62a
Maciej Rapacki <mac.rapacki@gmail.com>
2020-06-02 20:58:27 +0100

    - add route from 'react-router-dom' - use route in header to handle images - add images - create style for Header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import '../style/Header.css';
+
+import homeImg from '../images/home.jpg';
+import productsImg from '../images/products.jpg';
+import contactImg from '../images/contact.jpg';
+import adminImg from '../images/admin.jpg';
+import errorImg from '../images/error.jpg'
+
 
 const Header = () => {
     return (
-        <div>header</div>
+        <>
+            <Switch>
+                <Route path="/" exact render={() => (
+                    <img src={homeImg} alt='code' />
+                )} />
+                <Route path="/products" render={() => (
+                    <img src={productsImg} alt='code' />
+                )} />
+                <Route path="/contact" render={() => (
+                    <img src={contactImg} alt='code' />
+                )} />
+                <Route path="/admin" render={() => (
+                    <img src={adminImg} alt='code' />
+                )} />
+                <Route render={() => (
+                    <img src={errorImg} alt='code' />
+                )} />
+            </Switch>
+
+        </>
     );
 }
 
--- src/layout/Page.js
+++ src/layout/Page.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Route, Switch, } from 'react-router-dom'
 
 const Page = () => {
     return (
-        <div>page</div>
+        <>
+            <Switch>
+                <Route exact path='/' render={() => (
+                    <h1>Witaj</h1>
+                )} />
+                <Route path='/products' render={() => (
+                    <h1>products</h1>
+                )} />
+                <Route path='/contact' render={() => (
+                    <h1>concact</h1>
+                )} />
+                <Route path='/admin' render={() => (
+                    <h1>admin panel</h1>
+                )} />
+                <Route render={() => (
+                    <h1>smoething wrong</h1>
+                )} />
+            </Switch>
+        </>
     );
 }
 
